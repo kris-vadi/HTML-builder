a3816f280e3d2b79050d965271be9d0c39458165
Kristina Morozova <christina.vadi@gmail.com>
2023-05-08 11:37:57 +0600

    fix: exclude directory in 03 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,15 +3,16 @@ const fs = require("fs");
 
 const dirPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(dirPath, {withFileTypes: true}, (err, files) => {
+fs.readdir(dirPath, {withFileTypes: true}, (err, content) => {
   if (err) throw err;
-  files.filter((f) => f.isFile());
-
+  const files = content.filter((f) => f.isFile());
+  
   files.forEach((file) => {
     const name = file.name;
     const extention = path.extname(file.name);
     const filePath = path.join(dirPath, file.name);
 
+    
     const size = fs.stat(filePath, (err, stats) => { 
       console.log(`${name} - ${extention} - ${stats.size}b`);
     })
