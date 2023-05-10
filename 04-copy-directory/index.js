419b1ec736fb7987d7dc953f930d1eea0dbaa649
Kristina Morozova <christina.vadi@gmail.com>
2023-05-10 19:50:39 +0600

    fix: 04 task if delete file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,21 +5,24 @@ const path = require('path');
 const src = path.join(__dirname, 'files');
 const newDir = path.join(__dirname, 'files-copy');
 
-fsPromises.mkdir(newDir, { recursive: true }).then(
+fsPromises.rm(newDir, { recursive: true, force: true}).finally( function() {
 
-  fs.readdir(src, {withFileTypes: true}, (err, files) => {
-    if (err) throw err;
+  fsPromises.mkdir(newDir, { recursive: true }).then(
+    
+    fs.readdir(src, {withFileTypes: true}, (err, files) => {
+      if (err) throw err;
+      
+      files.forEach((file) => {
+        if (file.isFile()) {
+          fs.copyFile(path.join(src, file.name) , path.join(newDir, file.name), (err) => {
+            if (err) throw err;
+          })
+        }
+      })
 
-    files.forEach((file) => {
-      if (file.isFile()) {
-        fs.copyFile(path.join(src, file.name) , path.join(newDir, file.name), (err) => {
-          if (err) throw err;
-        })
-      }
     })
 
-  })
-
-).catch(function() {
-  console.log('failed to create directory');
+  ).catch(function() {
+    console.log('failed to create directory');
+  });
 });
